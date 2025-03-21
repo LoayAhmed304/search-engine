fbc7c76830ad92c544cecccc95d0d436809d6b51
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-03-22 01:12:22 +0200

    init: add starter files

---
+++ src/main/java/com/project/SearchEngine/SearchEngineApplication.java
@@ -0,0 +1,14 @@
+package com.project.SearchEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchEngineApplication.class, args);
+		System.err.println("Hello my Search Engine!");
+	}
+
+}
