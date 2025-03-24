70665d4a52afd878e1c2f245cc4d3c64dfb47b61
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-03-24 23:15:48 +0200

    build: add mongodb driver dependency

---
+++ src/main/java/com/project/SearchEngine/database/repository/PageRepository.java
@@ -0,0 +1,9 @@
+package com.project.SearchEngine.database.repository;
+import org.springframework.data.mongodb.repository.*;
+import org.springframework.stereotype.Repository;
+
+import com.project.SearchEngine.database.model.Page;
+
+// public class PageRepository extends MongoRepository<Page, String> {
+
+// } 
--- src/main/java/com/project/repository/PageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.repository;
-import org.springframework.data.mongodb.repository.*;
-import org.springframework.stereotype.Repository;
-import com.project.model.Page;
-
-public class PageRepository extends MongoRepository<Page, String> {
-
-} 
