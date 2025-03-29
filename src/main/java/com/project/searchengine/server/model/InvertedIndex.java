2fe3909081ac57dcd683f2f0377876732614577b
Habiba <habibaayman2004@outlook.com>
2025-03-29 12:32:20 +0200

    style: auto-formatted files

--- src/main/java/com/project/searchengine/server/model/InvertedIndex.java
+++ src/main/java/com/project/searchengine/server/model/InvertedIndex.java
@@ -1,8 +1,8 @@
 package com.project.searchengine.server.model;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.*;
-
 
 @Document(collection = "words")
 public class InvertedIndex {
--- src/main/java/com/project/searchengine/server/repository/PageRepository.java
+++ src/main/java/com/project/searchengine/server/repository/PageRepository.java
@@ -4,13 +4,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.project.searchengine.server.model.Page;
 
-
 public interface PageRepository extends MongoRepository<Page, String> {
 
     // @Query("{ 'url' : ?0 }")
     // Page findByUrl(String url);
 
-
     // void createPage(Page page);
 
-} 
+}
