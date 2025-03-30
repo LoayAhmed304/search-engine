2d3571fdb4bff741d4380c870aedab648360d898
Loay Ahmed <loayahmed304@gmail.com>
2025-03-30 06:23:53 +0200

    feat(ranker): add main ranking loop and token score processor

--- src/main/java/com/project/searchengine/ranker/RankCalculator.java
+++ src/main/java/com/project/searchengine/ranker/RankCalculator.java
@@ -1,5 +1,33 @@
 package com.project.searchengine.ranker;
 
+import com.project.searchengine.server.model.PageReference;
+
 public class RankCalculator {
 
+    private static final double TF_IDF_COEFF = 0.7;
+    private static final double PAGE_RANK_COEFF = 0.3;
+
+    /**
+     * Calculates the weighted Term Frequency
+     * TF(weighted) = number of token occurence in document / total tokens in document
+     *
+     * @param pr: PageReference that the token is part of
+     * @return normalized TF value (double)
+     */
+    public static double calculateTF(PageReference pr) {
+        return 1.2;
+    }
+
+    /**
+     * Calculates the final score to be used for ranking
+     * Score = (weight) * TF * IDF + (1-weight) * page rank
+     *
+     * @param tf: weighted term frequency
+     * @param idf: normalized IDF
+     * @param pageRank: rank of the page relative to other pages
+     * @return the final score to be merged with scores Map (double)
+     */
+    public static double calculateScore(double tf, double idf, double pageRank) {
+        return 0.3;
+    }
 }
--- src/main/java/com/project/searchengine/ranker/Ranker.java
+++ src/main/java/com/project/searchengine/ranker/Ranker.java
@@ -1,10 +1,69 @@
 package com.project.searchengine.ranker;
 
+import com.project.searchengine.server.model.PageReference;
+import com.project.searchengine.server.service.PageService;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Ranker {
-    public Ranker(String query, Map<String, List<String>> wordResults){
 
+    private final List<String> queryTokens;
+    private final Map<String, List<PageReference>> wordResults;
+    private final long totalDocuments;
+    private final PageService pageService;
+
+    public Ranker(String query, Map<String, List<PageReference>> wordResults) {
+        this.queryTokens = Arrays.asList(query.split("\\s+"));
+        this.wordResults = wordResults;
+        this.pageService = new PageService();
+        this.totalDocuments = pageService.getTotalDocuments();
+    }
+
+    /**
+     * The main function, which handles the ranking for all the given results
+     *
+     * @return ordered ranked list of the results pages IDs
+     */
+    public List<String> rank() {
+        Map<String, Double> scores = new HashMap<>();
+        for (String token : queryTokens) {
+            processToken(token, scores);
+        }
+
+        return null; // should return sorted page ids (strings) according to their values in scores Map
+    }
+
+    /**
+     * Processes a single token's score
+     *
+     * @param token: the token (word) desired to process
+     * @param scores: Map object by reference, to update the total score of every page (<pageId, score>)
+     */
+    private void processToken(String token, Map<String, Double> scores) {
+        List<PageReference> prs = wordResults.getOrDefault(token, Collections.emptyList());
+
+        double idf = getIDF(prs.size());
+        for (PageReference pr : prs) {
+            double pageRank = pr.getPageRank();
+
+            double tf = RankCalculator.calculateTF(pr);
+            double score = RankCalculator.calculateScore(tf, idf, pageRank);
+
+            String pageId = pr.getPageId();
+            scores.merge(pageId, score, Double::sum);
+        }
+    }
+
+    /**
+     * Simple computation for the normalized IDF value
+     *
+     * @param docsWithToken: number of documents contianing the token
+     * @return normalized IDF value (double)
+     */
+    private double getIDF(int docsWithToken) {
+        return 0;
     }
 }
