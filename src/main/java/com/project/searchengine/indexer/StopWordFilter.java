b072dc3dbb4bf76384db378c573429f497f13c81
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-04-15 13:23:05 +0200

    feat(indexer): read stop words from the file

--- src/main/java/com/project/searchengine/indexer/StopWordFilter.java
+++ src/main/java/com/project/searchengine/indexer/StopWordFilter.java
@@ -1,18 +1,25 @@
 package com.project.searchengine.indexer;
 
+import java.io.IOException;
+import java.nio.file.*;
 import java.util.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-@PropertySource("classpath:stopwords.properties")
 @Component
 public class StopWordFilter {
 
     private final Set<String> stopWords;
 
-    public StopWordFilter(@Value("${stop-words}") String stopWords) {
-        this.stopWords = new HashSet<>(Arrays.asList(stopWords.split(",")));
+    public StopWordFilter() {
+        try {
+            this.stopWords = new HashSet<>(
+                Files.readAllLines(Paths.get("src/main/resources/stopwords.txt"))
+            );
+        } catch (IOException e) {
+            System.err.println("Error loading stop words: " + e.getMessage());
+            throw new RuntimeException("Failed to load stop words", e);
+        }
+
         System.out.println("Stop words loaded: " + this.stopWords + "\n");
         System.out.println("Stop words size: " + this.stopWords.size() + "\n");
     }
