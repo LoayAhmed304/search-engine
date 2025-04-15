f88773d55846a4ed4423c60021c4c153bcb438d4
helana <helananady7@gmail.com>
2025-04-15 23:47:41 +0200

    fix: remove deleted imports

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,11 +2,6 @@
   <header>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
   </header>
 
