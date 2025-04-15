74f7a55d398eb34d9c849f0d4dfd110cb9d0896e
Tasneemmhammed0 <tasneemmohammed098@gmail.com>
2025-04-15 11:22:14 +0200

    feat(indexer): implement stop word filter class

--- src/main/java/com/project/searchengine/indexer/StopWordFilter.java
+++ src/main/java/com/project/searchengine/indexer/StopWordFilter.java
@@ -1,12 +1,22 @@
 package com.project.searchengine.indexer;
 
-import java.util.HashSet;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 @PropertySource("classpath:stopwords.properties")
+@Component
 public class StopWordFilter {
 
-    HashSet<String> stopWords = new HashSet<>();
+    Set<String> stopWords;
 
-    public StopWordFilter() {}
+    public StopWordFilter(@Value("${stop-words}") String stopWords) {
+        this.stopWords = new HashSet<>(Arrays.asList(stopWords.split(",")));
+        System.out.println("Stop words loaded: " + this.stopWords);
+    }
+
+    public boolean isStopWord(String token) {
+        return stopWords.contains(token);
+    }
 }
