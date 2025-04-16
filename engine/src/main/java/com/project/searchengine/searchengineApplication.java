60b5d7430ab43d460691475a13e839e6bb1e624b
Habiba <habibaayman2004@outlook.com>
2025-04-16 23:20:41 +0200

    refactor(crawler): commented crawling running in main

--- engine/src/main/java/com/project/searchengine/searchengineApplication.java
+++ engine/src/main/java/com/project/searchengine/searchengineApplication.java
@@ -1,24 +1,25 @@
 package com.project.searchengine;
 
-import com.project.searchengine.crawler.Crawler;
+// import com.project.searchengine.crawler.Crawler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+// import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class searchengineApplication {
 
     public static void main(String[] args) {
         // Start the Spring Boot application context
-        ApplicationContext context = SpringApplication.run(searchengineApplication.class, args);
+        SpringApplication.run(searchengineApplication.class, args);
+        // ApplicationContext context = SpringApplication.run(searchengineApplication.class, args);
         System.out.println("Hello my Search Engine!");
 
         // Test the crawler by seeding the frontier
-        Crawler crawler = context.getBean(Crawler.class);
-        long start = System.currentTimeMillis();
-        // crawler.seed();
-        crawler.crawl();
-        long duration = System.currentTimeMillis() - start;
-        System.out.println("retrieving from the frontier took: " + duration + " ms");
+        // Crawler crawler = context.getBean(Crawler.class);
+        // long start = System.currentTimeMillis();
+        // // crawler.seed();
+        // crawler.crawl();
+        // long duration = System.currentTimeMillis() - start;
+        // System.out.println("retrieving from the frontier took: " + duration + " ms");
     }
 }
