7ed305bccf80a5a809f876b309c44191b2100796
helana <helananady7@gmail.com>
2025-04-17 15:45:49 +0200

    feat(client): add base button component

--- client/src/components/ui/BaseButton.vue
+++ client/src/components/ui/BaseButton.vue
@@ -1,7 +1,27 @@
 <template>
+  <button class="base-button" @click="$emit('button-click')">
+    <slot />
+  </button>
 </template>
 
 <script setup>
+const emit = defineEmits(['button-click']);
+
 </script>
 
-<style scoped></style>
+<style scoped>
+.base-button {
+  background-color: var(--accent-color);
+  border: none;
+  padding: 10px 16px;
+  font-size: 1.2rem;
+  /* margin: 1rem ; */
+  cursor: pointer;
+  border-radius: 5px;
+  color: var(--background-color);
+  opacity: 0.85;
+}
+.base-button:hover {
+  opacity: 1;
+}
+</style>
