387083caf4255c2783f96fdc8b951595f0b0528b
Loay Ahmed <loayahmed304@gmail.com>
2025-04-18 01:07:56 +0200

    feat(PagesRank): add the main single page rank iteration

--- engine/src/main/java/com/project/searchengine/ranker/PageRank.java
+++ engine/src/main/java/com/project/searchengine/ranker/PageRank.java
@@ -2,40 +2,80 @@ package com.project.searchengine.ranker;
 
 import com.project.searchengine.server.model.Page;
 import com.project.searchengine.server.model.UrlDocument;
-import com.project.searchengine.server.repository.UrlsFrontierRepository;
 import com.project.searchengine.server.service.PageService;
 import com.project.searchengine.server.service.UrlsFrontierService;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class PageRank {
 
+    private static final double DAMPING_FACTOR = 0.8;
+
     private final UrlsFrontierService urlFrontier;
-    private final List<UrlDocument> allUrls;
-    private final List<Page> allPages;
+    private final Map<String, UrlDocument> allUrls;
+    private final Map<String, Page> allPages;
     private final PageService pageService;
 
     public PageRank(UrlsFrontierService urlFrontier, PageService pageService) {
         this.urlFrontier = urlFrontier;
         this.pageService = pageService;
 
-        this.allUrls = this.urlFrontier.getAllUrls();
-        this.allPages = pageService.getAllPages();
+        this.allUrls = this.urlFrontier.getAllUrls()
+            .stream()
+            .collect(Collectors.toMap(UrlDocument::getNormalizedUrl, Function.identity()));
+        this.allPages = this.pageService.getAllPages()
+            .stream()
+            .collect(Collectors.toMap(Page::getUrl, Function.identity()));
     }
 
+    /**
+     * Computes the rank for each page in the database (only 1 run)
+     * @return status boolean (to be updated later)
+     */
     public boolean computePagesRank() {
         Map<String, List<String>> incomingLinks = computeIncomingLinks();
-        initializePagesRank();
+
+        if (!initializePagesRank()) {
+            return false;
+        }
+
+        Map<String, Double> newRanks = new HashMap<>();
+
+        for (Page page : allPages.values()) {
+            String url = page.getUrl();
+            double curRank = 0;
+
+            for (String incoming : incomingLinks.getOrDefault(url, List.of())) {
+                Page incomingPage = allPages.get(incoming);
+                int outLinks = allUrls
+                    .getOrDefault(incoming, new UrlDocument())
+                    .getLinkedPages()
+                    .size();
+                if (outLinks > 0) curRank += incomingPage.getRank() / outLinks;
+            }
+
+            curRank *= DAMPING_FACTOR;
+            curRank += 1 - DAMPING_FACTOR;
+            newRanks.put(url, curRank);
+        }
+
+        for (Page page : allPages.values()) {
+            page.setRank(newRanks.get(page.getUrl()));
+        }
+
         return true;
     }
 
     /**
      * Sets all pages rank initially to 1 / N
      * N: Total number of pages in the database
+     * @return status boolean
      */
     boolean initializePagesRank() {
         int N = allPages.size();
 
-        for (Page page : allPages) {
+        for (Page page : allPages.values()) {
             page.setRank((double) 1 / N);
         }
 
@@ -50,7 +90,7 @@ public class PageRank {
         Map<String, List<String>> incomingLinks = new HashMap<>();
 
         // for every page, add it to the outgoing links from the url frontier
-        for (UrlDocument urlDoc : allUrls) { // loop over every url in the url frontier (not all necessarily crawled)
+        for (UrlDocument urlDoc : allUrls.values()) { // loop over every url in the url frontier (not all necessarily crawled)
             String curUrl = urlDoc.getNormalizedUrl(); // extract its url
             List<String> outgoingPagesUrls = urlDoc.getLinkedPages(); // extract its outgoing links array
 
