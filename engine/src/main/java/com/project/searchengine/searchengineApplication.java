116bad00a72a0ad320ddfe6f1b508163c83c5f41
Loay Ahmed <loayahmed304@gmail.com>
2025-04-19 23:42:02 +0200

    feat(pagerank): modify PageRank module to update rank in UrlFrontier collection instead of Page collection

--- engine/src/main/java/com/project/searchengine/searchengineApplication.java
+++ engine/src/main/java/com/project/searchengine/searchengineApplication.java
@@ -1,5 +1,8 @@
 package com.project.searchengine;
 
+import com.project.searchengine.ranker.PageRank;
+import com.project.searchengine.server.service.PageService;
+import com.project.searchengine.server.service.UrlsFrontierService;
 // import com.project.searchengine.crawler.Crawler;
 
 import org.springframework.boot.SpringApplication;
@@ -14,6 +17,24 @@ public class searchengineApplication {
         ApplicationContext context = SpringApplication.run(searchengineApplication.class, args);
         System.out.println("Hello my Search Engine!");
 
+        UrlsFrontierService urlsFrontierService = context.getBean(UrlsFrontierService.class);
+        PageService pageService = context.getBean(PageService.class);
+
+        System.out.println(
+            "\n\n\n\n\t\t***************Starting PageRank calculation***************"
+        );
+        long startTime = System.currentTimeMillis();
+        PageRank pageRank = new PageRank(urlsFrontierService, pageService);
+        boolean success = pageRank.computeAllRanks();
+        long duration = System.currentTimeMillis() - startTime;
+
+        if (success) {
+            System.out.println("PageRank calculation completed successfully!");
+            System.out.println("Pages ranked: " + urlsFrontierService.getAllUrls().size());
+            System.out.println("Time taken: " + duration + "ms\n\n");
+        } else {
+            System.out.println("PageRank calculation failed!\n\n");
+        }
         // Test the crawler by seeding the frontier
         // Crawler crawler = context.getBean(Crawler.class);
         // long start = System.currentTimeMillis();
